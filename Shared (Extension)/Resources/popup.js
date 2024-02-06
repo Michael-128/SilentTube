1857c3bed92a42f0161caf8421298842e1edb32a
Michael-128 <michal290704@gmail.com>
2024-02-06 20:18:02 +0100

    improve hiding elements

--- Shared (Extension)/Resources/popup.js
+++
@@ -1,4 +0,0 @@
-const rootElement = document.querySelector("div.content")
-
-rootElement.querySelector("input#hideHomePage").addEventListener("change", () => { console.log("change") })
-console.log("hello")
